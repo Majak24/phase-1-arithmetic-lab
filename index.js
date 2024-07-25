d7cb30d5cde9adf5ff3971978dafe93b6dd504c8
Majak Deng <majakbanks.deng>
2024-07-25 14:51:24 +0300

    modified

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
 // Write your code here
+// Challenge 1: Multiply
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+
+// Challenge 2: Random integer greater than 0
+const random = Math.floor(Math.random() * 10) + 1;
+
+// Challenge 3: Modulus operation
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4;
+
+// Challenge 4: Finding the maximum value
+const max = Math.max(5, 10, 15, 20);
